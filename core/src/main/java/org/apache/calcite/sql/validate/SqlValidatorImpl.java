1a3d37103320eb27cfb93cb425e8e08d4ba662d4
Minji Kim <minji@dremio.com>
2017-01-14 22:43:08 -0800

    [CALCITE-1954] Column from outer join should be null, whether or not it is aliased
    Close apache/calcite#522

--- core/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
+++ core/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
@@ -1998,7 +1998,7 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
             usingScope,
             alias,
             new AliasNamespace(this, call, enclosingNode),
-            false);
+            forceNullable);
       }
       return node;
     case MATCH_RECOGNIZE:

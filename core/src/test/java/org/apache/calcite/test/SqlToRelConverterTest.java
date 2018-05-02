9eb89cad5224c72197b5f5022929471f897913c4
chunhui-shi <cshi@maprtech.com>
2018-05-03 02:32:12 +0300

    [CALCITE-2291] (part 2) Fix star case

--- core/src/test/java/org/apache/calcite/test/SqlToRelConverterTest.java
+++ core/src/test/java/org/apache/calcite/test/SqlToRelConverterTest.java
@@ -2500,6 +2500,20 @@ public class SqlToRelConverterTest extends SqlToRelTestBase {
     sql(sql3).with(getTesterWithDynamicTable()).ok();
   }
 
+  @Test public void testStarDynamicSchemaUnnest() {
+    final String sql3 = "select * \n"
+        + "from SALES.CUSTOMER as t1,\n"
+        + "lateral (select t2.fake_col2 as fake_col3\n"
+        + "         from unnest(t1.fake_col) as t2) as t3";
+    sql(sql3).with(getTesterWithDynamicTable()).ok();
+  }
+
+  @Test public void testStarDynamicSchemaUnnest2() {
+    final String sql3 = "select * \n"
+        + "from SALES.CUSTOMER as t1,\n"
+        + "unnest(t1.fake_col) as t2";
+    sql(sql3).with(getTesterWithDynamicTable()).ok();
+  }
   /**
    * Test case for Dynamic Table / Dynamic Star support
    * <a href="https://issues.apache.org/jira/browse/CALCITE-1150">[CALCITE-1150]</a>
